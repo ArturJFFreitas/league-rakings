849b63a134c8cd70b93a12e0db06cf1c34ccc267
Artur Freitas <arturfreitas09@gmail.com>
2026-08-05 19:07:22 +0100

    Update scraping function to filter columns
    Filtered DataFrame to keep only specified columns before sorting.

--- src/opta.py
+++ src/opta.py
@@ -2,7 +2,20 @@ import pandas as pd
 
 URL = "https://dataviz.theanalyst.com/opta-power-rankings/league-meta.json"
 
+KEEP_COLUMNS = ["globalRank",
+                "leagueName",
+                "countryName",
+                "confederationName",
+                "seasonAverageRating",
+                "top10Rating",
+                "top5Rating",
+                "leagueSize",
+                "countrySize",
+                "confederationRank",
+                "lastWeekGlobalRank",
+                "endDate"]
+
 def scrape_opta_league_rankings():
     df = pd.read_json(URL)
-    df = df.sort_values("globalRank").reset_index(drop=True)
+    df = df[KEEP_COLUMNS].sort_values("globalRank").reset_index(drop=True)
     return df
