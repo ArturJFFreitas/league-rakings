823aacd44045613a8d9f335f36c951b5790b4c99
Artur Freitas <arturfreitas09@gmail.com>
2026-08-05 19:08:08 +0100

    Refactor club rankings release function call

--- src/scrape.py
+++ src/scrape.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from opta import scrape_opta_league_rankings
-from utils import (add_timestamp_cols, create_or_update_club_rankings_release)
+from utils import add_timestamp_cols, create_or_update_release
 
 current_time = datetime.now()
 
 df = scrape_opta_league_rankings()
 df = add_timestamp_cols(df, current_time)
 
-create_or_update_club_rankings_release(df=df,file_name="opta-league-rankings.csv")
+create_or_update_release(df=df, file_name="opta-league-rankings.csv)
