f028e1bd4b35dbadffd6df56ae1146ca73fbb15b
Artur Freitas <arturfreitas09@gmail.com>
2026-08-06 13:11:07 +0100

    Format create_or_update_release call for readability

--- src/scrape.py
+++ src/scrape.py
@@ -7,4 +7,7 @@ current_time = datetime.now()
 df = scrape_opta_league_rankings()
 df = add_timestamp_cols(df, current_time)
 
-create_or_update_release(df=df, file_name="opta-league-rankings.csv,  )
+create_or_update_release(
+    df=df,
+    file_name="opta-league-rankings.csv",
+)
